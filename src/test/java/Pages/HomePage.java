563b5ec1c3f31bb0238bd7f6000d71075403c839
miroslavculicQA <miroslav.culic90@gmail.com>
2024-02-04 19:49:21 +0100

    Replaced locators for cards

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -6,22 +6,26 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class HomePage extends BaseTest {
 
     public HomePage() {
         PageFactory.initElements(driver, this);
     }
 
-
-    // TODO IZMENI LOKATOR ZA ELEMENTS KARTICU
-
-    @FindBy(xpath = "//*[@id=\"app\"]/div/div/div[2]/div/div[1]/div/div[3]/h5")
-    public WebElement elementsCard;
-
+    @FindBy(css = ".card.mt-4.top-card")
+    public List<WebElement> cards;
 
     // -----------------------------
-    public void clickOnElementsCard() {
-        elementsCard.click();
+
+    public void clickOnCard(String cardName) {
+        for (int i = 0; i < cards.size(); i++) {
+            if (cards.get(i).getText().equalsIgnoreCase(cardName)) {
+                cards.get(i).click();
+                break;
+            }
+        }
     }
 
 
