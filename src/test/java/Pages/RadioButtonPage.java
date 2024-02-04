05c1841fbd1e15dfb7a9ebbe2a6598f4258966ad
miroslavculicQA <miroslav.culic90@gmail.com>
2024-02-04 19:50:41 +0100

    Added locators

--- src/test/java/Pages/RadioButtonPage.java
+++ src/test/java/Pages/RadioButtonPage.java
@@ -1,2 +1,34 @@
-package Pages;public class RadioButtonPage {
+package Pages;
+
+import Base.BaseTest;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class RadioButtonPage extends BaseTest {
+    public RadioButtonPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "yesRadio")
+    public WebElement yesRadioButton;
+    @FindBy(id = "impressiveRadio")
+    public WebElement impressiveRadioButton;
+    @FindBy(id = "noRadio")
+    public WebElement noRadioButton;
+    @FindBy(className = "mt-3")
+    public WebElement message;
+    @FindBy(className = "text-success")
+    public WebElement selectedButtonText;
+
+    /*@FindBy(className = ".custom-control-label.disabled")
+    public WebElement disabledRadioButton;
+*/
+    // ---------------------------------
+
+    public void testClick() {
+        noRadioButton.click();
+    }
+
+
 }
