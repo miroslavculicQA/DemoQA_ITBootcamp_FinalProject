133b32cde6853374e596b4006afd9eb5636d13c3
miroslavculicQA <miroslav.culic90@gmail.com>
2024-02-04 19:52:42 +0100

    Added tests

--- src/test/java/Tests/TextBoxTests.java
+++ src/test/java/Tests/TextBoxTests.java
@@ -1,61 +1,54 @@
 package Tests;
 
 import Base.BaseTest;
+import Pages.RadioButtonPage;
 import Pages.SidebarPage;
 import Pages.HomePage;
 import Pages.TextBoxPage;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class TextBoxTests extends BaseTest {
-
     @BeforeMethod
     public void pageSetUp() {
-
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-
-        driver.manage().window().maximize();
-//        driver.get("https://demoqa.com/");
-        driver.get("https://demoqa.com/elements");
-
         homePage = new HomePage();
         sidebarPage = new SidebarPage();
         textBoxPage = new TextBoxPage();
-        sidebarPage.clickOnSideBarItem("Text Box");
 
-        // TODO PREBACI SVE IZ BASE TESTA OVDE
+        homePage.clickOnCard("Elements");
+        clickOn_Elements_TextBox();     // Clicks on an item from Elements menu in sidebar
 
     }
 
-//    @Test
-//    public void userCanClickToElementsCard() {
-//        homePage.clickOnElementsCard();
-//    }
 
     @Test
-    public void userCanClickToElementsItem() {
-        sidebarPage.clickOnSideBarItem("Text Box");
+    public void userCanFillTextBoxes_AndSubmitFormWithValidData() {
+        for (int i = 1; i <= excelReader.getLastRow("TextBox"); i++) {
+            String validFullName = excelReader.getStringData("TextBox", i, 0);
+            String validEmail = excelReader.getStringData("TextBox", i, 1);
+            String validCurrentAddress = excelReader.getStringData("TextBox", i, 2);
+            String validPermanentAddress = excelReader.getStringData("TextBox", i, 3);
+
+            textBoxPage.inputFullName(validFullName);
+            textBoxPage.inputEmail(validEmail);
+            textBoxPage.inputCurrentAddress(validCurrentAddress);
+            textBoxPage.inputPermanentAddress(validPermanentAddress);
+
+            textBoxPage.clickOnSubmitButton();
+
+            Assert.assertEquals(textBoxPage.output.get(0).getText(), "Name:" + validFullName);
+            Assert.assertEquals(textBoxPage.output.get(1).getText(), "Email:" + validEmail);
+            Assert.assertEquals(textBoxPage.output.get(2).getText(), "Current Address :" + validCurrentAddress);
+            Assert.assertEquals(textBoxPage.output.get(3).getText(), "Permananet Address :" + validPermanentAddress);
+        }
+
     }
 
-    @Test
-    public void userCanSubmitFormWithOnlyOneInputFieldFiled() {
-        textBoxPage.inputFullName("Miroslav");
-        textBoxPage.clickOnSubmitButton();
-
-        // TODO uradi asertaciju
-        /*
-        String outputText = output.getText();
-        String expectedOutputText = "Name:" + fullName + "\nEmail:" + email + "\nCurrent Address :" + currentAddress + "\nPermananet Address :" + permanentAddress;
-        Assert.assertEquals(outputText, expectedOutputText);
-        */
-
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 
 
