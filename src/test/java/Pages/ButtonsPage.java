5c2667e4da79f3923734902849763076097fd2e5
miroslavculicQA <miroslav.culic90@gmail.com>
2024-04-10 18:56:00 +0200

    Added expected text Strings

--- src/test/java/Pages/ButtonsPage.java
+++ src/test/java/Pages/ButtonsPage.java
@@ -2,11 +2,8 @@ package Pages;
 
 import Base.BaseTest;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import java.util.List;
 
 public class ButtonsPage extends BaseTest {
 
