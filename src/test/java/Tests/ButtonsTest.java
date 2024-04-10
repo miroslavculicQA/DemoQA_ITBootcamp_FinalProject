f9106a556e5a1f0579e130fa2b73a57bb8a7f125
miroslavculicQA <miroslav.culic90@gmail.com>
2024-04-10 18:54:24 +0200

    Separate test for each button

--- src/test/java/Tests/ButtonsTest.java
+++ src/test/java/Tests/ButtonsTest.java
@@ -4,8 +4,8 @@ import Base.BaseTest;
 import Pages.ButtonsPage;
 import Pages.HomePage;
 import Pages.SidebarPage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,27 +18,35 @@ public class ButtonsTest extends BaseTest {
         buttonsPage = new ButtonsPage();
 
         homePage.clickOnCard("Elements");
-        clickOn_Elements_Buttons();     // Clicks on an item from Elements menu in sidebar
+        clickOn_Elements_Buttons();     // Clicks on item from Elements menu in sidebar
     }
 
-    @Test
-    public void userCanClickOnAllButtons() throws InterruptedException {
-        Thread.sleep(2000);
-
+    @Test (priority = 10)
+    public void userCanClickOnDoubleClickButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(buttonsPage.doubleClickMeButton));
         action_doubleClick(buttonsPage.doubleClickMeButton);
         Assert.assertTrue(buttonsPage.doubleClickMessage.isDisplayed());
+        Assert.assertEquals(buttonsPage.doubleClickMessageExpectedText, buttonsPage.doubleClickMessageText());
+    }
 
+    @Test (priority = 20)
+    public void userCanClickOnRightClickButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(buttonsPage.rightClickMeButton));
         action_rightClick(buttonsPage.rightClickMeButton);
         Assert.assertTrue(buttonsPage.rightClickMessge.isDisplayed());
+        Assert.assertEquals(buttonsPage.rightClickMessageExpectedText, buttonsPage.rightClickMessageText());
+    }
 
+    @Test (priority = 30)
+    public void userCanClickOnClickMeButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(buttonsPage.clickMeButton));
         buttonsPage.clickOnClickMeButton();
-        Assert.assertTrue((buttonsPage.dynamicClickMessage.isDisplayed()));
-
+        Assert.assertTrue(buttonsPage.dynamicClickMessage.isDisplayed());
+        Assert.assertEquals(buttonsPage.clickMessageExpectedText, buttonsPage.clickMessageText());
     }
 
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
+
+
+
 
 }
